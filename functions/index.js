9816b8ffc26c92814b5940ac4e7bfa05912ab78c
tucker reisig <tuckerreisig@Tuckers-MacBook-Pro.local>
2020-07-21 23:32:22 -0400

    Querying API Endpoint for 10 skills max

--- functions/index.js
+++ functions/index.js
@@ -90,5 +90,128 @@ app.post("/newUser", async (req, res) => {
   }
 });
 
+// Trashy query endpoint for 10 filters max
+app.post("/queryStudents", async (req, res) => {
+  let data;
+  switch (req.body.numberOfFilters) {
+    case "1":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .get();
+      break;
+    case "2":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .get();
+      break;
+    case "3":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .where(req.body.filter.name3, "==", req.body.filter.value3)
+        .get();
+
+      break;
+    case "4":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .where(req.body.filter.name3, "==", req.body.filter.value3)
+        .where(req.body.filter.name4, "==", req.body.filter.value4)
+        .get();
+
+      break;
+    case "5":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .where(req.body.filter.name3, "==", req.body.filter.value3)
+        .where(req.body.filter.name4, "==", req.body.filter.value4)
+        .where(req.body.filter.name5, "==", req.body.filter.value5)
+        .get();
+
+      break;
+    case "6":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .where(req.body.filter.name3, "==", req.body.filter.value3)
+        .where(req.body.filter.name4, "==", req.body.filter.value4)
+        .where(req.body.filter.name5, "==", req.body.filter.value5)
+        .where(req.body.filter.name6, "==", req.body.filter.value6)
+        .get();
+
+      break;
+    case "7":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .where(req.body.filter.name3, "==", req.body.filter.value3)
+        .where(req.body.filter.name4, "==", req.body.filter.value4)
+        .where(req.body.filter.name5, "==", req.body.filter.value5)
+        .where(req.body.filter.name6, "==", req.body.filter.value6)
+        .where(req.body.filter.name7, "==", req.body.filter.value7)
+        .get();
+
+      break;
+    case "8":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .where(req.body.filter.name3, "==", req.body.filter.value3)
+        .where(req.body.filter.name4, "==", req.body.filter.value4)
+        .where(req.body.filter.name5, "==", req.body.filter.value5)
+        .where(req.body.filter.name6, "==", req.body.filter.value6)
+        .where(req.body.filter.name7, "==", req.body.filter.value7)
+        .where(req.body.filter.name8, "==", req.body.filter.value8)
+        .get();
+
+      break;
+    case "9":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .where(req.body.filter.name3, "==", req.body.filter.value3)
+        .where(req.body.filter.name4, "==", req.body.filter.value4)
+        .where(req.body.filter.name5, "==", req.body.filter.value5)
+        .where(req.body.filter.name6, "==", req.body.filter.value6)
+        .where(req.body.filter.name7, "==", req.body.filter.value7)
+        .where(req.body.filter.name8, "==", req.body.filter.value8)
+        .where(req.body.filter.name9, "==", req.body.filter.value9)
+        .get();
+      break;
+    case "10":
+      data = await firestore
+        .collection("students")
+        .where(req.body.filter.name1, "==", req.body.filter.value1)
+        .where(req.body.filter.name2, "==", req.body.filter.value2)
+        .where(req.body.filter.name3, "==", req.body.filter.value3)
+        .where(req.body.filter.name4, "==", req.body.filter.value4)
+        .where(req.body.filter.name5, "==", req.body.filter.value5)
+        .where(req.body.filter.name6, "==", req.body.filter.value6)
+        .where(req.body.filter.name7, "==", req.body.filter.value7)
+        .where(req.body.filter.name8, "==", req.body.filter.value8)
+        .where(req.body.filter.name9, "==", req.body.filter.value9)
+        .where(req.body.filter.name10, "==", req.body.filter.value10)
+        .get();
+      break;
+
+    default:
+      break;
+  }
+  const docs = data.docs.map((doc) => doc.data());
+  res.send(docs);
+});
+
 // Base API endpoint
 exports.api = functions.https.onRequest(app);

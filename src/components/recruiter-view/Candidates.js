b4f8cd2a1fbb9cf6e91b20678c04afc106e95b0f
Adam Winek <adamwinek@gmail.com>
2020-07-24 15:11:26 -0400

    Added invisible scrollbars

--- src/components/recruiter-view/Candidates.js
+++ src/components/recruiter-view/Candidates.js
@@ -3,16 +3,17 @@ import CandidateCard from "./CandidateCard"
 
 function Candidates (props) {
     
+    console.log(props.candidateCards)
     return (
         <div style={{width:'100%'}}>
             <h1 className="recruiterViewHeader BreeSerif" style={{width: '100%'}}> Candidates</h1>
-            <div className="d-flex recruiterViewCardDiv">
-                {props.candidateCards.map( Candidate =>(
-                            <CandidateCard toggleResumeView={(candidate) => props.toggleResumeView(candidate)} key={Candidate.Email} info={Candidate}/>
 
-                ))}
-                
-            </div>
+                <div className="d-flex recruiterViewCardDiv">
+                    {props.candidateCards.map( Candidate =>(
+                                <CandidateCard toggleResumeView={(candidate) => props.toggleResumeView(candidate)} key={Candidate.Email} info={Candidate}/>
+
+                    ))}
+                </div>
             
 
         </div>

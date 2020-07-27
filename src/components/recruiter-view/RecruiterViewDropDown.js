805ebefc5c14d65d2f2f44bac818f33939ba460e
Sai Gongidi <saigongidi@gmail.com>
2020-07-27 00:05:03 -0400

    Update RecruiterViewDropDown.js

--- src/components/recruiter-view/RecruiterViewDropDown.js
+++ src/components/recruiter-view/RecruiterViewDropDown.js
@@ -1,72 +1,78 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 
-// The type property allows you to switch between "ResumeView" and "Notes" dropdowns 
-//The primary difference between these two is the type of content that goes inside the expanded container
-// The Notes type container contains an <textfield> tag allowing recruiters to leave comments on a profile
-// The ResumeView type container contains <h1> tags
-// The text prop controls the content of the dropdown header
-// the collapsedIcon and expandedIcon  props allow you to pass in custom icons which change along with the state of the container
-// the items prop (expects an array) is used to control the inner content of the dropdown down for the "ResumeView" dropdown
+function RecruiterViewDropDown(props) {
+    const [collapsed, setCollapsed] = useState(false);
+    let itemArray = []
 
+    if (props.items !== null && props.items !== undefined) {
+        Object.keys(props.items).forEach((key, index) => {
+            if (props.items[key] === true) {
+                itemArray.push(key);
+            }
 
-function RecruiterViewDropDown (props) {
-    const [collapsed, setCollapsed] = useState(true);
+        })
 
-
-    if (props.type === "ResumeView"){
-        if(!collapsed){
-            return(
-                <div className="resumeViewDropDownDiv" onClick={() => setCollapsed(true)} >
-                    <h1 className="BreeSerif resumeViewDropDownText" > {props.text} </h1>
-                    {props.collapsedIcon}
-                </div>
-            );
-        }  else {
-            return(
-                <div>
-                    <div className="resumeViewDropDownDiv" onClick={() => setCollapsed(false)}>
-                        <h1 className="BreeSerif resumeViewDropDownText" > {props.text} </h1>
-                        {props.expandedIcon}
-                    </div>
-                    <div className="d-flex justify-content-start p-10px"> 
-                        {props.items.map(item => (
-                        
-                            <h1 className="recruiterViewItems BreeSerif "> {item} </h1>
-                            
-                        ))}
-                    </div>
-                </div>
-                
-                )
-        }
-    } else if (props.type === "Notes") {
-        if(!collapsed){
-            return(
-                <div className="resumeViewDropDownDiv" onClick={() => setCollapsed(true)} >
-                    <h1 className="BreeSerif resumeViewDropDownText" > {props.text} </h1>
-                    {props.collapsedIcon}
-                </div>
-            );
-        }  else {
-            return(
-                <div>
-                    <div className="resumeViewDropDownDiv" onClick={() => setCollapsed(false)}>
-                        <h1 className="BreeSerif resumeViewDropDownText" > {props.text} </h1>
-                        {props.expandedIcon}
-                    </div>
-                    <div className="d-flex justify-content-start "> 
-                        {props.items.map(item => (
-                        
-                            <textarea className='resumeViewNotes' > </textarea>
-                            
-                        ))}
-                    </div>
-                </div>
-                
-                )
-        }
     }
-        
-}  
+    let expandedView = null;
+    let icon = <AddIcon  className="resumeViewDropDownIcon" />
+
+
+    if (!collapsed) {
+        expandedView = (
+
+            <div className="d-flex justify-content-start resumeViewItems">
+
+                {itemArray.map(item => (
+
+                    <h1 className="recruiterViewItems BreeSerif "> {item} </h1>
+
+                ))}
+            </div>
+
+
+        );
+        icon = (
+        <RemoveIcon className="resumeViewDropDownIcon"/>        
+            )
+    }
+
+
+    // let collapsedView = (
+    //     <div className="resumeViewDropDownDiv" onClick={() => setCollapsed(true)} >
+    //         <h1 className="BreeSerif resumeViewDropDownText" > {props.text} </h1>
+    //         {props.collapsedIcon}
+    //     </div>
+    // );
+
+
+    // let expandedView = (
+    //     <div>
+    //         <div className="resumeViewDropDownDiv" onClick={() => setCollapsed(false)}>
+    //             <h1 className="BreeSerif resumeViewDropDownText" > {props.text} </h1>
+    //         </div>
+    //     </div>
+    // );
+
+    console.log(itemArray)
+    if (props.items === null || itemArray.length === 0) {
+        return null
+    } else {
+        return (
+            <div>
+                <div className="resumeViewDropDownDiv" onClick={() => setCollapsed(!collapsed)} >
+                    <h1 className="BreeSerif resumeViewDropDownText" > {props.text} </h1>
+                    {icon}
+                </div>
+                {expandedView}
+            </div>
+
+
+        )
+
+    }
+
+}
 
 export default RecruiterViewDropDown
